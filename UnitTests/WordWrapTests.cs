8a386bc207e765a3f4af62a80db16624c0c1352b
Patrick Campanale <patrickappcreator@gmail.com>
2013-12-28 18:17:03 -0600

    We now have more tests passing. YAY US!

--- UnitTests/WordWrapTests.cs
+++ UnitTests/WordWrapTests.cs
@@ -8,12 +8,39 @@ namespace UnitTests
     public class WordWrapTests
     {
         [TestMethod]
-        public void CanSplitWords()
+        public void CanGetZeroWords()
+        {
+            WordWrapService service = new WordWrapService();
+            string words = service.GetWords("’Twas brillig, and the slithy toves Did gyre and gimble in the wabe", 0);
+
+            Assert.AreEqual("", words);
+        }
+
+        [TestMethod]
+        public void CanGetOneWord()
         {
             WordWrapService service = new WordWrapService();
             string words = service.GetWords("’Twas brillig, and the slithy toves Did gyre and gimble in the wabe", 1);
 
             Assert.AreEqual("’Twas", words);
+        }
+
+        [TestMethod]
+        public void CanGetTwoWords()
+        {
+            WordWrapService service = new WordWrapService();
+            string words = service.GetWords("’Twas brillig, and the slithy toves Did gyre and gimble in the wabe", 2);
+
+            Assert.AreEqual("’Twas brillig,", words);
+        }
+
+        [TestMethod]
+        public void CanGetThreeWords()
+        {
+            WordWrapService service = new WordWrapService();
+            string words = service.GetWords("’Twas brillig, and the slithy toves Did gyre and gimble in the wabe", 3);
+
+            Assert.AreEqual("’Twas brillig, and", words);
         }
     }
 }
--- WP8App/Services/WordWrapService.cs
+++ WP8App/Services/WordWrapService.cs
@@ -9,8 +9,18 @@ namespace WPAppStudio.Services
     {
         public string GetWords(string text, int wordCount)
         {
-            int space = text.IndexOf(' ');
-            return text.Substring(0, space);
+
+            StringBuilder result = new StringBuilder();
+
+            for (int word = 0; word < wordCount; word++)
+            {
+                int space = text.IndexOf(' ', 1);
+                //return text.Substring(0, space);
+                result.Append(text.Substring(0, space));
+                text = text.Substring(space);
+            }
+
+            return result.ToString();
         }
     }
 }
